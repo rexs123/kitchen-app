e3130353bd8daf4cde9384e34bc7ab0b7c088ec8
Rama <me@rexsdev.com>
2022-08-20 18:56:56 -0400

    npm dependancies & vite configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,14 @@
 import axios from 'axios';
-window.axios = axios;
+import '@fortawesome/fontawesome-pro/js/all'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+
+const app = createApp({})
+app.use(createPinia())
+app.config.globalProperties.$axios = Axios;
+
+app.mount('#app')
