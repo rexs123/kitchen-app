edd012e17b88a9ceb76f4c024a326f30275c5ced
Rama <me@rexsdev.com>
2022-08-21 07:52:20 -0400

    vite & vue config tweaks

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 import '@fortawesome/fontawesome-pro/js/all'
-import { createApp } from 'vue'
+import { createApp } from 'vue/dist/vue.esm-bundler.js'
 import { createPinia } from 'pinia'
 
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-
 const app = createApp({})
 app.use(createPinia())
-app.config.globalProperties.$axios = Axios;
+app.config.globalProperties.$axios = axios;
 
 app.mount('#app')
